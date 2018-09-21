f1aaad4d0e7ee258f8c81f91b2c52adad2bd860f
胡峰 <hufeng@shensz.cn>
2018-09-21 11:40:40 +0800

    Initial Commit

---
+++ FFExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFExtension
+//
+//  Created by hufeng on 21/9/18.
+//  Copyright © 2018年 shensz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFExtension/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFExtension
+//
+//  Created by hufeng on 21/9/18.
+//  Copyright © 2018年 shensz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
